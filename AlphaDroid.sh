830dd98170d51f6f194133760c7d3704d820916f
Murtaza <murtazakolachi7@gmail.com>
2025-04-30 03:13:06 +0500

    Initial commit

---
+++ AlphaDroid.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+rm -rf .repo/local_manifests; \
+rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+rm -rf packages/apps/ViPER4AndroidFX && \
+repo init --depth=1 --no-repo-verify -u https://github.com/AlphaDroid-Project/manifest -b alpha-15.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b alpha device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+export BUILD_USERNAME=Murtaza; \
+export BUILD_HOSTNAME=crave; \
+export TZ=Asia/Karachi; \
+# Vanilla Build
+. build/envsetup.sh && \
+breakfast apollo user && make installclean && mka bacon; \
+rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
+cd out/target/product && mv apollo vanilla && cd ../../..; \
+# Gapps Build
+cd device/xiaomi/apollo && rm alpha_apollo.mk && mv gapps.txt alpha_apollo.mk && cd ../../..; \
+. build/envsetup.sh; \
+breakfast apollo user && make installclean && mka bacon; \
+cd out/target/product && mv apollo gapps && cd ../../..; \
