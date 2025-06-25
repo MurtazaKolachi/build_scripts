3bf3d573ab46f306adf57499845566f4c9831a45
Murtaza <murtazakolachi7@gmail.com>
2025-06-26 01:05:54 +0500

    TMP

--- Axion.sh
+++ Axion.sh
@@ -2,24 +2,21 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-rm -rf packages/apps/ViPER4AndroidFX && \
+rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf hardware/dolby; \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b axion device/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b test device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b test vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://github.com/Matrixx-Devices/hardware_dolby -b sony-1.3 hardware/dolby && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
 /opt/crave/resync.sh && \
 export BUILD_USERNAME=Murtaza; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Karachi; \
 # Vanilla Build
 . build/envsetup.sh && \
-gk -s && axion apollo va user && make installclean && ax -br; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
-cd out/target/product && mv apollo vanilla && cd ../../..; \
-# Gapps Build
-. build/envsetup.sh; \
-gk -s && axion apollo gms pico user && make installclean && ax -br; \
-cd out/target/product && mv apollo gapps && cd ../../..; \
+gk -s && axion apollo va user && make installclean && ax -br
