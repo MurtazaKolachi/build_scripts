73c7996dbf3c2c82839950353d1473e4521e9757
Murtaza <murtazakolachi7@gmail.com>
2025-06-29 08:08:10 +0500

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -9,21 +9,17 @@ git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b test
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi/ -b lineage-23.0 hardware/xiaomi && \
 rm -rf hardware/dolby && \
-git clone https://github.com/Matrixx-Devices/hardware_dolby -b sony-1.3 hardware/dolby && \
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
 rm -rf packages/apps/ViPER4AndroidFX && \
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
 rm -rf packages/resources/devicesettings && \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
+git clone https://github.com/MurtazaKolachi/device_xiaomi_camera -b main device/xiaomi/camera && \
+git clone https://gitlab.com/murtazakolachi/vendor_xiaomi_camera -b aosp-15-apollo vendor/xiaomi/camera && \
 /opt/crave/resync.sh && \
 export BUILD_USERNAME=Murtaza; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Karachi; \
 # Vanilla Build
 . build/envsetup.sh && \
-gk -s && axion apollo va user && make installclean && ax -br; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
-cd out/target/product && mv apollo vanilla && cd ../../..; \
-# Gapps Build
-. build/envsetup.sh; \
-gk -s && axion apollo gms pico user && make installclean && ax -br; \
-cd out/target/product && mv apollo gapps && cd ../../..; \
+gk -s && axion apollo va user && make installclean && ax -br
