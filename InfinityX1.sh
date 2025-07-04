6c6829a0e72d42077b5e1160238b0f04993b1330
Murtaza <murtazakolachi7@gmail.com>
2025-07-04 14:40:05 +0500

    Create InfinityX1.sh

---
+++ InfinityX1.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# Remove Manifests
+rm -rf .repo/local_manifests; \
+
+# ROM Repo
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectInfinity-X/manifest -b 15 --git-lfs -g default,-mips,-darwin,-notdefault && \
+
+# Sync Rom
+/opt/crave/resync.sh && \
+
+# Trees
+
+# Device Tree
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b infinity device/xiaomi/apollo && \
+
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b new vendor/xiaomi/apollo && \
+
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
+
+# Dolby
+rm -rf hardware/dolby
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
+
+# ViPER
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+
+# Other
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
+
+# Build Environment
+export BUILD_USERNAME=Murtaza; \
+export BUILD_HOSTNAME=crave; \
+export TZ=Asia/Karachi; \
+
+# Vanilla Build
+. build/envsetup.sh && \
+lunch infinity_apollo-user && make installclean && mka bacon; \
+rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
+cd out/target/product && mv apollo vanilla && cd ../../..; \
+
+# Gapps Build
+cd device/xiaomi/apollo && rm infinity_apollo.mk && mv gapps.txt infinity_apollo.mk && cd ../../..; \
+. build/envsetup.sh; \
+lunch infinity_apollo-user && make installclean && mka bacon; \
+cd out/target/product && mv apollo gapps && cd ../../..; \
