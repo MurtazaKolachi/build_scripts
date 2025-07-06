6a5435efd21439b454a4163467abf516aea9284d
Murtaza <murtazakolachi7@gmail.com>
2025-07-06 11:48:37 +0500

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -1,26 +1,73 @@
 #! /bin/bash
 
-rm -rf .repo/local_manifests; \
-rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/MurtazaKolachi/manifest -b bka --git-lfs -g default,-mips,-darwin,-notdefault && \
+# Remove Manifests
+rm -rf .repo/local_manifests
+
+# ROM Repo
+repo init --depth=1 --no-repo-verify -u https://github.com/Mi-Apollo/manifest -b bka --git-lfs -g default,-mips,-darwin,-notdefault && \
+
+# Sync Rom
 /opt/crave/resync.sh && \
+
+# Trees
+
+# Device Tree
+rm -rf device/xiaomi
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b evo device/xiaomi/apollo && \
+
+# Vendor Tree
+rm -rf vendor/xiaomi
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b evo vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
-git clone https://github.com/MurtazaKolachi/device_xiaomi_camera -b main device/xiaomi/camera && \
-git clone https://gitlab.com/murtazakolachi/vendor_xiaomi_camera -b aosp-15-apollo vendor/xiaomi/camera && \
+
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+
+# Dolby
+rm -rf hardware/dolby
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
+
+# ViPER
+rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
-export BUILD_USERNAME=Murtaza; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Karachi; \
-# Vanilla Build
+
+# Other
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
+
+# --- Setup Build Environment ---
+export BUILD_USERNAME=Murtaza
+export BUILD_HOSTNAME=crave
+export TZ=Asia/Karachi
+
+# --- Vanilla Build ---
+echo "Starting Vanilla Build..."
 . build/envsetup.sh && \
-lunch lineage_apollo-bp1a-user && make installclean && m evolution; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
-cd out/target/product && mv apollo vanilla && cd ../../..; \
-# Gapps Build
-cd device/xiaomi/apollo && rm lineage_apollo.mk && mv gapps.txt lineage_apollo.mk && cd ../../..; \
-. build/envsetup.sh; \
-lunch lineage_apollo-bp1a-user && make installclean && m evolution; \
-cd out/target/product && mv apollo gapps && cd ../../..; \
+lunch lineage_apollo-bp2a-user && \
+make installclean && \
+m evolution && \
+
+# --- Handle Vanilla Output ---
+echo "Handling Vanilla build output..."
+mv out/target/product/apollo out/target/product/vanilla && \
+echo "Vanilla build finished. Output is in out/target/product/vanilla"
+
+# --- Gapps Build ---
+echo "Setting up for Gapps Build..."
+mv device/xiaomi/apollo/gapps.txt device/xiaomi/apollo/lineage_apollo.mk && \
+
+echo "Starting Gapps Build..."
+make installclean && \
+m evolution && \
+
+# --- Handle Gapps Output ---
+echo "Handling Gapps build output..."
+mv out/target/product/apollo out/target/product/gapps
+echo "Gapps build finished. Output is in out/target/product/gapps"
+
+# --- Build is Successful!! ---
+echo "All builds completed successfully!"
