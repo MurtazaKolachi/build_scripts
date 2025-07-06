8ff487769799c4fa89934c82fb4754fcdec8cddf
Murtaza <murtazakolachi7@gmail.com>
2025-07-06 11:28:42 +0500

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -2,24 +2,6 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-
-# ==== Fix for Trusty Soong Error (dummy module define) ====
-
-mkdir -p trusty/vendor/google/aosp/scripts/dummy
-
-cat > trusty/vendor/google/aosp/scripts/Android.bp << 'EOF'
-bootstrap_go_package {
-    name: "trusty_dirgroup_prebuilts_clang_host_linux-x86",
-    pkgPath: "trusty/vendor/google/aosp/scripts/dummy",
-    deps: [],
-    srcs: ["dummy.go"],
-}
-EOF
-
-echo "package dummy" > trusty/vendor/google/aosp/scripts/dummy/dummy.go
-
-# ==== End of Trusty Fix ====
-
 repo init --depth=1 --no-repo-verify -u https://github.com/MurtazaKolachi/manifest -b bka --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b evo device/xiaomi/apollo && \
