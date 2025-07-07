58635b0679448a97761d4e6301632a2d805e1a55
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-07-07 19:31:24 +0500

    Update CrDroid

--- CrDroid.sh
+++ CrDroid.sh
@@ -1,17 +1,50 @@
 #! /bin/bash
 
-rm -rf .repo/local_manifests; \
-rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-rm -rf packages/apps/ViPER4AndroidFX && \
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -g default,-mips,-darwin,-notdefault && \
+# Remove Manifests
+rm -rf .repo/local_manifests
+
+# ROM Repo
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+
+# Sync Rom
 /opt/crave/resync.sh && \
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b main device/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+
+# Trees
+
+# Device Tree
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b new device/xiaomi/apollo && \
+
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b new vendor/xiaomi/apollo && \
+
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+
+# Dolby
+rm -rf hardware/dolby
+git clone https://github.com/MurtazaKolachi/hardware_dolby -b sony-1.3 hardware/dolby && \
+
+# ViPER
+rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
-export BUILD_USERNAME=Murtaza; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Karachi; \
+
+# Other
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
+
+
+# --- Setup Build Environment ---
+export BUILD_USERNAME=Murtaza
+export BUILD_HOSTNAME=crave
+export TZ=Asia/Karachi
+
+# --- Build ---
 . build/envsetup.sh && \
 breakfast apollo user && make installclean && mka bacon
