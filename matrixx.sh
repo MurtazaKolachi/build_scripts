0fa46357400b860e6071895b1c473534f03c2acf
Murtaza Hussain <murtazakolachi7@gmail.com>
2025-09-03 22:23:37 +0500

    Use build manifest

--- matrixx.sh
+++ matrixx.sh
@@ -9,39 +9,14 @@
 rm -rf .repo/local_manifests
 
 # --- Init ROM repo ---
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs && \
+
+# --- Clone Manifest---
+git clone https://github.com/MurtazaKolachi/build_manifest -b matrixx .repo/local_manifests && \
 
 # --- Sync ROM ---
 #/opt/crave/resync.sh && \
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-# --- Clone Device Tree ---
-rm -rf device/xiaomi
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b matrixx device/xiaomi/apollo && \
-
-# --- Clone Vendor Tree ---
-rm -rf vendor/xiaomi
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 15 vendor/xiaomi/apollo && \
-
-# --- Clone Kernel Tree ---
-rm -rf kernel/xiaomi
-git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b aosp-15 kernel/xiaomi/apollo && \
-
-# --- Clone Hardware Tree ---
-rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
-
-# --- Dolby ---
-rm -rf hardware/dolby
-git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
-
-# --- ViPER ---
-rm -rf packages/apps/ViPER4AndroidFX
-git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
-
-# --- Device Settings ---
-rm -rf packages/resources/devicesettings
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-15 packages/resources/devicesettings && \
 
 # =============================
 #  Build: Vanilla → Gapps
@@ -62,7 +37,7 @@ mv out/target/product/apollo out/target/product/vanilla && \
 echo "===== Setting up for Gapps Build ====="
 mv device/xiaomi/apollo/gapps.txt device/xiaomi/apollo/lineage_apollo.mk && \
 make installclean && \
-mka bacon -j$(nproc --all) && \
+mka bacon && \
 mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/gapps.txt && \
 
 echo "===== Handling Gapps Output ====="
