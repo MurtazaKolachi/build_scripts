5f2edb92bb72909d6b96f7f1fe54de140dc7fa82
Murtaza Hussain <murtazakolachi7@gmail.com>
2025-09-09 20:42:29 +0500

    Update scripts
    Drop manifests use commands directly

--- matrixx.sh
+++ matrixx.sh
@@ -9,14 +9,39 @@
 rm -rf .repo/local_manifests
 
 # --- Init ROM repo ---
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs && \
-
-# --- Clone Manifest---
-git clone https://github.com/MurtazaKolachi/build_manifest -b matrixx .repo/local_manifests && \
+repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs && \
 
 # --- Sync ROM ---
 #/opt/crave/resync.sh && \
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# --- Clone Device Tree ---
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b matrixx device/xiaomi/apollo && \
+
+# --- Clone Vendor Tree ---
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+
+# --- Clone Kernel Tree ---
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
+
+# --- Clone Hardware Tree ---
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+
+# --- Dolby ---
+rm -rf hardware/dolby
+git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+
+# --- ViPER ---
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+
+# --- Device Settings ---
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
 
 # =============================
 #  Build: Vanilla → Gapps
