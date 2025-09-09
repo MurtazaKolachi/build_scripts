4904d957c9bb4a54fb8eeccf5116e998196b8481
Murtaza Hussain <murtazakolachi7@gmail.com>
2025-09-09 20:42:29 +0500

    Update scripts
    Drop manifests use commands directly

--- axion.sh
+++ axion.sh
@@ -9,14 +9,35 @@
 rm -rf .repo/local_manifests
 
 # --- Init ROM repo ---
-repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs && \
-
-# --- Clone Manifest---
-git clone https://github.com/MurtazaKolachi/build_manifest -b axion .repo/local_manifests && \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs && \
 
 # --- Sync ROM ---
 #/opt/crave/resync.sh && \
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# --- Clone Device Tree ---
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b axion device/xiaomi/apollo && \
+
+# --- Clone Vendor Tree ---
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+
+# --- Clone Kernel Tree ---
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
+
+# --- Clone Hardware Tree ---
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+
+# --- Dolby ---
+rm -rf hardware/dolby
+git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+
+# --- Device Settings ---
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
 
 # =============================
 #   Build: Vanilla → Gapps
--- infinity.sh
+++ infinity.sh
@@ -9,14 +9,39 @@
 rm -rf .repo/local_manifests
 
 # --- Init ROM repo ---
-repo init --depth=1 --no-repo-verify -u https://github.com/Mi-Apollo/infinity_manifest.git -b 16 --git-lfs && \
-
-# --- Clone Manifest---
-git clone https://github.com/MurtazaKolachi/build_manifest -b infinity .repo/local_manifests && \
+repo init -u https://github.com/Mi-Apollo/infinity_manifest.git -b 16 --git-lfs && \
 
 # --- Sync ROM ---
 #/opt/crave/resync.sh && \
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# --- Clone Device Tree ---
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b inf device/xiaomi/apollo && \
+
+# --- Clone Vendor Tree ---
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+
+# --- Clone Kernel Tree ---
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
+
+# --- Clone Hardware Tree ---
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+
+# --- Dolby ---
+rm -rf hardware/dolby
+git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+
+# --- ViPER ---
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX -b v4a packages/apps/ViPER4AndroidFX && \
+
+# --- Device Settings ---
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings -b aosp-16 packages/resources/devicesettings
 
 # =============================
 #  Build: Vanilla → Gapps
@@ -28,22 +53,22 @@ echo "===== Starting Vanilla Build ====="
 lunch infinity_apollo-user && \
 make installclean && \
 m bacon && \
-mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/vanilla.txt && \
+mv device/xiaomi/apollo/infinity_apollo.mk device/xiaomi/apollo/vanilla.txt && \
 
 echo "===== Handling Vanilla Output ====="
 mv out/target/product/apollo out/target/product/vanilla && \
 
 # --- Gapps Build ---
 echo "===== Setting up for Gapps Build ====="
-mv device/xiaomi/apollo/gapps.txt device/xiaomi/apollo/lineage_apollo.mk && \
+mv device/xiaomi/apollo/gapps.txt device/xiaomi/apollo/infinity_apollo.mk && \
 make installclean && \
 m bacon && \
-mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/gapps.txt && \
+mv device/xiaomi/apollo/infinity_apollo.mk device/xiaomi/apollo/gapps.txt && \
 
 echo "===== Handling Gapps Output ====="
 mv out/target/product/apollo out/target/product/gapps && \
 
 # --- Restore Vanilla ---
-mv device/xiaomi/apollo/vanilla.txt device/xiaomi/apollo/lineage_apollo.mk && \
+mv device/xiaomi/apollo/vanilla.txt device/xiaomi/apollo/infinity_apollo.mk && \
 
 echo "===== All builds completed successfully! ====="
