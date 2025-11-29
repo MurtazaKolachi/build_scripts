28a25e49003f0941e86d54c5cd2689bd0d943a56
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-11-29 16:10:29 +0500

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -10,20 +10,29 @@ rm -rf .repo/local_manifests
 rm -rf .repo/manifests
 rm -rf .repo/manifest.xml
 
+rm -rf device/xiaomi &&
+rm -rf vendor/xiaomi &&
+rm -rf kernel/xiaomi &&
+rm -rf hardware/xiaomi &&
+rm -rf hardware/dolby &&
+rm -rf packages/apps/ViPER4AndroidFX &&
+rm -rf packages/resources/devicesettings &&
+rm -rf vendor/evolution-priv/keys &&
+
 # --- Init ROM repo ---
-repo init -u https://github.com/Mi-Apollo/evo_manifest.git -b bka-q1 --git-lfs && \
+repo init -u https://github.com/Mi-Apollo/evo_manifest.git -b bq1 --git-lfs && \
 
 # --- Sync ROM ---
-#/opt/crave/resync.sh && \
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh && \
+# repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # --- Clone Device Tree ---
 rm -rf device/xiaomi
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b evo device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/device_xiaomi_apollo -b evo device/xiaomi/apollo && \
 
 # --- Clone Vendor Tree ---
 rm -rf vendor/xiaomi
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b new vendor/xiaomi/apollo && \
 
 # --- Clone Kernel Tree ---
 rm -rf kernel/xiaomi
@@ -31,11 +40,11 @@ git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b stag
 
 # --- Clone Hardware Tree ---
 rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
 
 # --- Dolby ---
-rm -rf hardware/dolby
-git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+# rm -rf hardware/dolby
+# git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
 
 # --- ViPER ---
 rm -rf packages/apps/ViPER4AndroidFX
