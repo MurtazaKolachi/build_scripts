f2935c9361bf8082d786cd6bd18a2142152d5ead
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-11 13:38:41 +0500

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -10,6 +10,18 @@ rm -rf .repo/local_manifests
 rm -rf .repo/manifests
 rm -rf .repo/manifest.xml
 
+# Better to use these to avoid any problems on crave
+rm -rf device/xiaomi &&
+rm -rf vendor/xiaomi &&
+rm -rf kernel/xiaomi &&
+rm -rf hardware/xiaomi &&
+rm -rf hardware/dolby &&
+rm -rf packages/apps/ViPER4AndroidFX &&
+rm -rf packages/resources/devicesettings &&
+rm -rf vendor/lineage-priv/keys &&
+rm -rf out/target/product/vanilla &&
+rm -rf out/target/product/gapps &&
+
 # --- Init ROM repo ---
 repo init -u https://github.com/Mi-Apollo/axion_android -b lineage-23.0 --git-lfs && \
 
@@ -19,23 +31,24 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # --- Clone Device Tree ---
 rm -rf device/xiaomi
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b axion device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/device_xiaomi_apollo -b axion device/xiaomi/apollo && \
 
 # --- Clone Vendor Tree ---
 rm -rf vendor/xiaomi
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
 
 # --- Clone Kernel Tree ---
 rm -rf kernel/xiaomi
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b staging kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
 
 # --- Clone Hardware Tree ---
 rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+# git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
 
 # --- Dolby ---
-rm -rf hardware/dolby
-git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+# rm -rf hardware/dolby
+# git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
 
 # --- Device Settings ---
 rm -rf packages/resources/devicesettings
