63a6d096fd459c0e91d1c47acbe09f1e79ea301d
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-12 18:31:56 +0500

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -72,23 +72,17 @@ echo "===== Starting Vanilla Build ====="
 axion apollo user va && \
 make installclean && \
 ax -br && \
-mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/vanilla.txt && \
 
 echo "===== Handling Vanilla Output ====="
 mv out/target/product/apollo out/target/product/vanilla && \
 
 # --- Gapps Build ---
 echo "===== Setting up for Gapps Build ====="
-mv device/xiaomi/apollo/gapps.txt device/xiaomi/apollo/lineage_apollo.mk && \
 axion apollo user gms pico && \
 make installclean && \
 ax -br && \
-mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/gapps.txt && \
 
 echo "===== Handling Gapps Output ====="
 mv out/target/product/apollo out/target/product/gapps && \
-
-# --- Restore Vanilla ---
-mv device/xiaomi/apollo/vanilla.txt device/xiaomi/apollo/lineage_apollo.mk && \
 
 echo "===== All builds completed successfully! ====="
