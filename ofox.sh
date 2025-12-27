dd490b3b33ac8b31495e398d00faee326acc0acb
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-27 20:12:55 +0500

    Add recovery scripts

---
+++ ofox.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Create Directory for OrangeFox
+mkdir OrangeFox
+cd OrangeFox
+
+# Just to be on safer side
+export OUT_DIR=out
+ulimit -n 16000
+
+# Clone and Sync OrangeFox
+git clone https://gitlab.com/OrangeFox/sync.git -b master
+cd sync
+./orangefox_sync.sh --branch 12.1 --path ~/OrangeFox/fox_12.1
+
+# Clone OrangeFox tree
+cd ~/OrangeFox/fox_12.1
+rm -rf device/xiaomi/apollo
+git clone https://github.com/murtazakolachi/device_xiaomi_ofox_apollo -b fox-12.1 ./device/xiaomi/apollo
+
+# Build Environment
+set +e
+source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+set -e
+lunch twrp_apollo-eng && make clean && mka adbd recoveryimage
