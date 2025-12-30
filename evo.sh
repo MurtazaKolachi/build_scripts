7bf1830c8d0a7e076f4eae2072fba68bc8810525
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-30 16:29:12 +0500

    Update scripts

--- evo.sh
+++ evo.sh
@@ -14,11 +14,11 @@ rm -rf .repo/manifest.xml
 rm -rf packages/resources/devicesettings
 
 # --- Init ROM repo ---
-repo init -u https://github.com/Mi-Apollo/evo_manifest.git -b bq1 --git-lfs && \
+repo init -u https://github.com/Mi-Apollo/evo_manifest.git -b bq2 --git-lfs && \
 
 # --- Sync ROM ---
-/opt/crave/resync.sh && \
-# repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+#/opt/crave/resync.sh && \
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # --- Clone Device Tree ---
 rm -rf device/xiaomi
@@ -31,14 +31,17 @@ git clone https://github.com/MurtazaKolachi/vendor_xiaomi_apollo -b 16 vendor/xi
 # --- Clone Kernel Tree ---
 rm -rf kernel/xiaomi
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
+#git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b staging kernel/xiaomi/apollo && \
 
 # --- Clone Hardware Tree ---
 rm -rf hardware/xiaomi
-git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+git clone https://github.com/MurtazaKolachi/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+#git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
 
 # --- Dolby ---
-# rm -rf hardware/dolby
-# git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+rm -rf hardware/dolby
+#git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+git clone https://github.com/Mi-Apollo/lunaris2_hardware_dolby -b 16.0 hardware/dolby && \
 
 # --- ViPER ---
 rm -rf packages/apps/ViPER4AndroidFX
@@ -67,7 +70,7 @@ rm -rf out/target/product/gapps &&
 # --- Vanilla Build ---
 echo "===== Starting Vanilla Build ====="
 . build/envsetup.sh && \
-lunch lineage_apollo-bp3a-user && \
+lunch lineage_apollo-bp4a-user && \
 make installclean && \
 m evolution && \
 mv device/xiaomi/apollo/lineage_apollo.mk device/xiaomi/apollo/vanilla.txt && \
