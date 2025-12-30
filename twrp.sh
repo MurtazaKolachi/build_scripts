1daa0e6aecb4117154fa2035f09eb202680ad377
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-12-30 16:29:12 +0500

    Update scripts

--- twrp.sh
+++ twrp.sh
@@ -8,10 +8,6 @@ cd twrp
 repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1 && \
 repo sync && \
 
-# Just to be on safer side
-export OUT_DIR=out && \
-ulimit -n 16000 && \
-
 # Clone TWRP tree
 rm -rf device/xiaomi/apollo && \
 git clone https://github.com/murtazakolachi/device_xiaomi_ofox_apollo -b twrp device/xiaomi/apollo && \
@@ -20,6 +16,10 @@ git clone https://github.com/murtazakolachi/device_xiaomi_ofox_apollo -b twrp de
 # Enable ccache
 # export USE_CCACHE=1
 # export CCACHE_EXEC=$(which ccache)
+
+# Just to be on safer side
+export OUT_DIR=out && \
+ulimit -n 16000 && \
 
 # Build Environment
 export ALLOW_MISSING_DEPENDENCIES=true
