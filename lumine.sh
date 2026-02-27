db02441bf9e26d5e820df309616d28f686b64ec0
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2026-02-27 20:26:22 +0500

    Create lumine.sh

---
+++ lumine.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# =============================
+#  LumineDroid Build Script
+# =============================
+
+# --- Remove old local manifests ---
+rm -rf .repo/local_manifests
+rm -rf .repo/manifests
+rm -rf .repo/manifest.xml
+
+# --- Remove Device Settings --- (Reason: It Will fail sync when we re run this script)
+rm -rf packages/resources/devicesettings
+
+# --- Init ROM repo ---
+repo init -u https://github.com/LumineDroid/platform_manifest -b bellflower --git-lfs && \
+
+# --- Sync ROM ---
+#/opt/crave/resync.sh && \
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+
+# --- Clone Device Tree ---
+rm -rf device/xiaomi
+git clone https://github.com/MurtazaKolachi/device_xiaomi_apollo -b lumine device/xiaomi/apollo && \
+
+# --- Clone Vendor Tree ---
+rm -rf vendor/xiaomi
+git clone https://github.com/MurtazaKolachi/vendor_xiaomi_apollo -b 16 vendor/xiaomi/apollo && \
+
+# --- Clone Kernel Tree ---
+rm -rf kernel/xiaomi
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b 16 kernel/xiaomi/apollo && \
+#git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b staging kernel/xiaomi/apollo && \
+
+# --- Clone Hardware Tree ---
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi && \
+#git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi && \
+
+# --- Dolby ---
+rm -rf hardware/dolby
+#git clone https://github.com/Mi-Apollo/hardware_dolby -b moto-1.0 hardware/dolby && \
+git clone https://github.com/Mi-Apollo/lunaris2_hardware_dolby -b 16.0 hardware/dolby && \
+
+# --- Device Settings ---
+rm -rf packages/resources/devicesettings
+git clone https://github.com/Mi-Apollo/android_packages_resources_devicesettings -b lineage-23.2 packages/resources/devicesettings && \
+
+# WFD repos
+# git clone https://github.com/PocoF3Releases/device_qcom_wfd device/qcom/wfd && \
+# git clone https://github.com/PocoF3Releases/vendor_qcom_wfd vendor/qcom/wfd && \
+
+# =============================
+#      Build Environment
+# =============================
+
+echo "===== Starting Vanilla Build ====="
+. build/envsetup.sh &&
+lunch apollo-bp4a-user &&
+make installclean &&
+mka bacon
+
+echo "===== Build completed successfully! ====="
